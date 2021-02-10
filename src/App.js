e5c585cdf2dfcb2146ff8f5381b92396c4abded7
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-02-09 22:32:16 -0500

    commit tree c93fff2444b8d117c824c4321c3a8959dc70bc02

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { Component }  from 'react';
+
+
+
+class App extends Component {
+
+    state = {
+        allPeople: []
+    }
+    
+    componentDidMount() {
+        return fetch('http://api.open-notify.org/astros.json')
+                .then(resp => resp.json())
+                .then(data => {
+                    this.setState({
+                        allPeople: data.people
+                    })
+                })
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.allPeople.map(person => person.name)}
+            </div>
+        )       
+    }
+}
+
+export default App
